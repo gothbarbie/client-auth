6b8c526f931f4657703921fe0e955319f7a297f5
Hanna Söderström <gothbarbie84@gmail.com>
2017-03-18 13:10:21 +0100

    adds action types and auth_reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { browserHistory } from 'react-router'
+import {
+  AUTH_USER
+} from './types'
 
 const API_URL = 'http://localhost:3090'
 
@@ -7,14 +11,22 @@ export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit to server
     axios.post(`${API_URL}/signin`, { email, password })
+      .then(response => {
+        // Good:
+        // - Update state to authenticated
+        dispatch({ type: AUTH_USER })
+        // - Save JWT token
+        // - Redirect to /feature
+        browserHistory.push('/feature')
+      })
+      .catch(() => {
+        // Bad:
+        // - Show error
 
-    // Good:
-    // - Update state to authenticated
-    // - Save JWT token
-    // - Redirect to /feature
+      })
 
-    // Bad:
-    // - Show error
+
+
   }
 
 
